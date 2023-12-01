8940485c57fe5e022245b3436120d900c072f3ab
[shinya1101T] <135099997+shinya1101T@users.noreply.github.com>
2023-12-01 21:20:48 +0900

    email add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(name: 'administrator', email: 'si20028@edu.aomori-u.ac.jp', password: 'takahashi', image_name: '1default_user.jpg', role: 1)
+User.create(name: 'administrator', email: 'si20028@edu.aomori-u.ac.jp', password: 'takahashi1101', image_name: '1default_user.jpg', role: 1)
