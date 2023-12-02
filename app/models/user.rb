a859bb79f77f4f1d7e8245500d7927ed2d9ae7c2
[shinya1101T] <135099997+shinya1101T@users.noreply.github.com>
2023-12-03 03:40:12 +0900

    password validate return

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
-    validates :password, presence: true,
-      format: { with: VALID_PASSWORD_REGEX,
-      message: "は半角6~12文字・英小文字・数字それぞれ１文字以上含む必要があります"}
     validates :name, {presence: true}
     validates :email, {presence: true,uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }}
     enum role: { general: 0, admin: 1 }
